4be7a5e84952392c53665a9f36780e5c215f1834
Hubert J <hubejan@gmail.com>
2017-05-05 17:41:04 -0400

    Working on HTML & CSS

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
-const db = new Sequelize("postgres://localhost:5432/TripPlanner", {
-    logging: true
+const db = new Sequelize("postgres://postgres:postgres@localhost:5432/TripPlanner", {
+    logging: console.log
 });
 
 module.exports = db;
